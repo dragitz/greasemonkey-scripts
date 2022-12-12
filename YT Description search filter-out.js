529f680710572cdd33759aef3de540c0f6c988b4
dragitz <gersinmc@gmail.com>
2022-12-12 13:39:05 +0100

    Update YT Description search filter-out.js

--- YT Description search filter-out.js
+++ YT Description search filter-out.js
@@ -1,12 +1,58 @@
 // ==UserScript==
 // @name         YT Description search filter-out
 // @namespace    https://github.com/dragitz/
-// @version      0.1
+// @version      0.2
 // @description  Filter out YouTube videos from search results that match a given pattern in the compact description
 // @author       dragitz
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
+
+
+
+
+
+
+
+infiniteLoop();  // start the infinite loop
+
+
+function infiniteLoop() {
+  setTimeout(function() {
+    // code to run in the loop goes here
+    
+    // test
+    deleteYtFormattedString(/(?=.*some WORDS HeRe)/i); // case-insensitive phrase
+    
+    // case-insensitive + order of words in the string doesn't matter, as long as we have >3 matches
+    deleteYtFormattedString(/(?=.*password)(?=.*link)(?=.*download)/i); 
+    
+    // example of websites used to spread malware
+    deleteYtFormattedString(/(?=.*festyy)/i);
+    deleteYtFormattedString(/(?=.*gestyy)/i);
+    deleteYtFormattedString(/(?=.*goo)/i);
+    deleteYtFormattedString(/(?=.*yoalizer)/i);
+    deleteYtFormattedString(/(?=.*blogspot)/i);
+    
+    // example of a more complex regex, must match them all
+    /*
+       - "download" followed by one or more alphanumeric characters and/or spaces, followed by the word "cheat"
+       - one or more alphanumeric characters and/or spaces, followed by the word "crack" and the word "download"
+       - "download" followed by one or more alphanumeric characters and/or spaces, followed by the words "mod apk"
+    */
+    deleteYtFormattedString(/^(download [a-zA-Z0-9\s]+ cheat|[a-zA-Z0-9\s]+ crack download|download [a-zA-Z0-9\s]+ mod apk)$/i);
+
+    // Character often present in videos that spread malware
+    deleteYtFormattedString(/(?=.*▬)/i);
+    
+
+    // Had to use a delay as the videos gets loaded after the user's page has finished loading the template (or something like this)
+    
+    infiniteLoop();
+  }, 1000); // ms
+}
+
+
 
 function deleteYtFormattedString(pattern) {
 
@@ -33,28 +79,31 @@ function deleteYtFormattedString(pattern) {
 
 
 
-function infiniteLoop() {
-  setTimeout(function() {
-    // code to run in the loop goes here
-    
-    // 
-    deleteYtFormattedString(/(?=.*password)(?=.*link)(?=.*download)/i);
-    deleteYtFormattedString(/(?=.*festyy)/i);
-    deleteYtFormattedString(/(?=.*gestyy)/i);
-    deleteYtFormattedString(/(?=.*goo)/i);
-    deleteYtFormattedString(/(?=.*yoalizer)/i);
-    deleteYtFormattedString(/(?=.*blogspot)/i);
-    deleteYtFormattedString(/(?=.*http)/i);
-    deleteYtFormattedString(/(?=.*https)/i);
-    deleteYtFormattedString(/(?=.*hxxp)/i);
-    deleteYtFormattedString(/^(download [a-zA-Z0-9\s]+ cheat|[a-zA-Z0-9\s]+ crack download|download [a-zA-Z0-9\s]+ mod apk)$/i);
-    deleteYtFormattedString(/(?=.*DOWNLOAD HERE)/i);
+// Function to Extract descriptions
+//
+// copy the function + call and paste it into the developer console
+function printYtFormattedString() {
+    var ytFormattedStrings = document.getElementsByClassName("metadata-snippet-text style-scope ytd-video-renderer");
+    for (var i = 0; i < ytFormattedStrings.length; i++) {
+        var formattedString = ytFormattedStrings[i].innerHTML;
 
-    deleteYtFormattedString(/(?=.*▬)/i);
+        // Remove html related stuff
+        formattedString = formattedString.replace("<span dir=\"auto\" class=\"style-scope yt-formatted-string\">", "");
+        formattedString = formattedString.replace("</span><span dir=\"auto\" class=\"bold style-scope yt-formatted-string\" style-target=\"bold\">", "");
+        formattedString = formattedString.replace("</span><span dir=\"auto\" class=\"style-scope yt-formatted-string\">", "");
+        formattedString = formattedString.replace("</span>", "");
+        formattedString = formattedString.replace("&nbsp;", "");
+				formattedString = formattedString.replace("<span dir=\"auto\" class=\"bold style-scope yt-formatted-string\" style-target=\"bold\">", "");
+        formattedString = formattedString.replace("</span><span dir=\"auto\" class=\"style-scope yt-formatted-string\">", "");
+        formattedString = formattedString.replace("</span>", "");
 
-    infiniteLoop();  // call the infiniteLoop() function again to continue the loop
-  }, 1000);  // the loop will run every 1000 milliseconds, or 1 second
+        // Print the updated string to the console
+        console.log(formattedString);
+
+    }
 }
 
-infiniteLoop();  // start the infinite loop
+printYtFormattedString()
+
+
 
