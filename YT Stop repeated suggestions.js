8d76373ab253329cd7d5d29d2a3f0b15d6639602
dragitz <gersinmc@gmail.com>
2023-05-07 14:32:48 +0200

    Update YT Stop repeated suggestions.js
    Default hide after is set to 4
    
    Clicking on a video resets that video's score
    
    Score can not go higher that the set max

--- YT Stop repeated suggestions.js
+++ YT Stop repeated suggestions.js
@@ -8,17 +8,23 @@
 (function() {
     'use strict';
 
-	const HIDE_AFTER = 15;
+	const HIDE_AFTER = 4;
     
-    const reloadRandomId = Math.floor((Math.random() * 900) + 1);
+    const reloadRandomId = Math.floor((Math.random() * 9000) + 1);
     const DB_NAME = 'seen_videos';
 
 	// type localStorage.clear() in console to clear your db
     let videoURLs = JSON.parse(localStorage.getItem(DB_NAME)) || {};
-
+	
+	// if we click on a video, we reset its score
+	if (videoURLs[window.location.href])
+	{		
+		videoURLs[window.location.href].score = 0;
+	}
+ 
+  	
     function storeVideoURL(url, reloadRandomId) {
         
-		// Check if link doesn't exist in our database
         if(!videoURLs[url])
         {
             videoURLs[url] = {
@@ -27,10 +33,11 @@
             };
             console.log("Added (new): ",url, " Score: ", videoURLs[url].score, " id: ",reloadRandomId);
 
-        }else
+        }
+        else
         {
             // only add score, if the reload id isn't the same as the current one
-            if (videoURLs[url].reloadRandomId !== reloadRandomId) {
+            if (videoURLs[url].reloadRandomId !== reloadRandomId && videoURLs[url].score < HIDE_AFTER) {
                 videoURLs[url].score = videoURLs[url].score + 1;
                 videoURLs[url].reloadRandomId = reloadRandomId;
                 console.log("Added (increased): ",url, " Score: ", videoURLs[url].score, " id: ",videoURLs[url].reloadRandomId);
@@ -45,8 +52,16 @@
         const videoRenderers = document.querySelectorAll('ytd-compact-video-renderer.style-scope.ytd-item-section-renderer');
         videoRenderers.forEach(videoRenderer => {
             const videoLink = videoRenderer.querySelector('a.yt-simple-endpoint.style-scope.ytd-compact-video-renderer');
-            if (videoLink && videoURLs[videoLink.href].score > HIDE_AFTER) {
+            if (videoLink && videoURLs[videoLink.href].score >= HIDE_AFTER) {
                 videoRenderer.remove();
+              
+              // remove glitched spinners
+              const spinners = document.querySelectorAll('tp-yt-paper-spinner.ytd-continuation-item-renderer');
+              spinners.forEach(element => {
+                const parentNode = element.parentNode;
+                parentNode.removeChild(element);
+              });
+
             }
         });
     }
